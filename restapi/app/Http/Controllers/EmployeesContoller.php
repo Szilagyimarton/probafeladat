4b8039dd60721de0cbf3f82f8339ad77d383098a
Szilagyimarton <szilagyimarton92@gmail.com>
2024-06-10 19:57:45 +0200

    updated loadxml function

--- restapi/app/Http/Controllers/EmployeesContoller.php
+++ restapi/app/Http/Controllers/EmployeesContoller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\DuplicateException;
 use App\Models\Employee;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -14,20 +15,21 @@ class EmployeesContoller extends Controller
     public function loadXML()
     {
         try {
+
+            DB::table('people')->truncate();
+
             $xmlFilePath = base_path('people.xml');
 
             $xml = simplexml_load_file($xmlFilePath);
-            $user = DB::table('people')->where('email', $xml->person[0]->email)->first();
-            if ($user) {
-                throw new Exception("XML is already added to database!", 400);
-            };
+
+
             foreach ($xml->person as $person) {
                 DB::insert('insert into people (name, email, dept, rank, phone, room) values (?, ?, ?, ?, ?, ?)', [$person->name, $person->email, $person->dept, $person->rank, $person->phone, $person->room]);
             }
 
             return response()->json(['message' => "XML is succesfully loaded to database!"]);
         } catch (Exception $e) {
-            return response()->json(['message' => $e->getMessage(), 'code' => $e->getCode()]);
+            return response()->json(['message' => 'An unexpected error occured!', 'code' => '500'], 500);
         }
     }
 
@@ -52,19 +54,16 @@ class EmployeesContoller extends Controller
                 'email' => 'required|email|unique:people',
                 'dept' => 'required',
                 'rank' => 'required',
-                'phone' => 'required|regex:/^\+36 \(1\) 666-\d{4}$/'
+                'phone' => 'required|regex:/^\+36 \(1\) 666-\d{4}$/',
+                'room' => 'nullable'
             ]);
-            if ($request['room']) {
-                $formfields['room'] = $request['room'];
-            } else {
-                $formfields['room'] = "Unknown";
-            }
+         
             Employee::create($formfields);
             return response()->json(['code' => '200', 'message' => $formfields['name'] . " is added to database!"], 200);
         } catch (ValidationException $e) {
             return response()->json(['code' => '400', 'message' => $e->getMessage()], 400);
         } catch (Exception $e) {
-            return response()->json(['message' => 'An unexpected error occurred!', 'code' => '500'], 500);
+            return response()->json(['message' => $e->getMessage(), 'code' => '500'], 500);
         }
     }
 
